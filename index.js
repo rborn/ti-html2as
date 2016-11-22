69e681397659a0fc6d0058a4b5dbe140e1d2926d
Konstantin Büschel <konstantin@die-interaktiven.de>
2016-11-22 11:59:37 +0100

    * added support for paragraph-tags * updated some code style * fixed Titanium SDK 6.0.0 iOS detection

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-var htmlparser = require("htmlparser2");
-var entities = require("entities");
+var htmlparser = require('htmlparser2'),
+	entities = require('entities'),
 
-// References to the full namespace to they get packaged for device builds
-var references = [
-  Ti.UI.AttributedString
-];
-
-var ns = Ti.UI;
+	// references to the full namespace to they get packaged for device builds
+	references = [
+  		Ti.UI.AttributedString
+	],
+	ns = Ti.UI,
+	osName = Ti.Platform.osname,
+	OS_IOS = !!~osName.search(/iphone/i) || !!~osName.search(/ipad/i) || !!~osName.search(/ios/i);
 
 if (parseInt(Ti.version.split('.')[0], 10) < 4) {
-  references.push(Ti.UI.iOS.AttributedString);
+	
+  references[references.length] = Ti.UI.iOS.AttributedString;
   ns = Ti.UI.iOS;
 }
 
-var ios = Ti.Platform.name === 'iPhone OS';
 
 // Custom matcher is a function to enable parsing of extra node types.
 // @return Object
@@ -44,14 +45,18 @@ var ios = Ti.Platform.name === 'iPhone OS';
 function walker(node, parameters, outerFont, customMatcher) {
 
   if (node.type === 'text') {
+    
     parameters.text += entities.decodeHTML(node.data);
-
-  } else if (node.type === 'tag' && node.children) {
+  } 
+  else if (node.type === 'tag' && node.children) {
+  	
     var innerFont;
 
     // clone font property from wrapping tags
     if (outerFont) {
+    	
       innerFont = {};
+      
       outerFont.fontWeight && (innerFont.fontWeight = outerFont.fontWeight);
       outerFont.fontFamily && (innerFont.fontFamily = outerFont.fontFamily);
       outerFont.fontSize && (innerFont.fontSize = outerFont.fontSize);
@@ -59,17 +64,20 @@ function walker(node, parameters, outerFont, customMatcher) {
 
     // override font properties from this tag
     if (node.name === 'strong' || node.name === 'b') {
+      
       innerFont || (innerFont = {});
       innerFont.fontWeight = 'bold';
-
-    } else if (node.name === 'font' && node.attribs) {
+    } 
+    else if (node.name === 'font' && node.attribs) {
 
       if (node.attribs.face) {
+        
         innerFont || (innerFont = {});
         innerFont.fontFamily = node.attribs.face;
       }
 
       if (node.attribs.size) {
+        
         innerFont || (innerFont = {});
         innerFont.fontSize = node.attribs.size;
       }
@@ -80,6 +88,7 @@ function walker(node, parameters, outerFont, customMatcher) {
 
     // walk children
     node.children.forEach(function onEach(child) {
+    	
       parameters = walker(child, parameters, innerFont, customMatcher);
     });
 
@@ -87,15 +96,18 @@ function walker(node, parameters, outerFont, customMatcher) {
     var length = parameters.text.length - offset;
 
     if (typeof customMatcher === 'function') {
+      
       var customMatch = customMatcher(node, parameters, outerFont, offset, length, ns);
 
       if (customMatch.parameters === undefined || customMatch.continue === undefined) {
+        
         throw new Error('customMatcher should return an object with parameters and continue properties defined');
       }
 
       parameters = customMatch.parameters;
 
       if (customMatch.continue === false) {
+        
         return parameters;
       }
     }
@@ -104,90 +116,115 @@ function walker(node, parameters, outerFont, customMatcher) {
     if (length > 0) {
 
       if (node.name === 'a' && node.attribs && node.attribs.href) {
+        
         parameters.attributes.unshift({
+          
           type: ns.ATTRIBUTE_LINK,
           value: node.attribs.href,
           range: [offset, length]
         });
-
-      } else if (node.name === 'u') {
+      } 
+      else if (node.name === 'u') {
+      	
         parameters.attributes.unshift({
+          
           type: ns.ATTRIBUTE_UNDERLINES_STYLE,
-          value: ios ? ns.ATTRIBUTE_UNDERLINE_STYLE_SINGLE : undefined,
+          value: OS_IOS ? ns.ATTRIBUTE_UNDERLINE_STYLE_SINGLE : undefined,
           range: [offset, length]
         });
-
-      } else if (ios && (node.name === 'i' || node.name === 'em')) {
+      } 
+      else if (OS_IOS && (node.name === 'i' || node.name === 'em')) {
+        
         parameters.attributes.unshift({
+          
           type: ns.ATTRIBUTE_OBLIQUENESS,
           value: 0.25,
           range: [offset, length]
         });
-
-      } else if (node.name === 'strike' || node.name === 'del' || node.name === 's') {
+      } 
+      else if (node.name === 'strike' || node.name === 'del' || node.name === 's') {
+        
         parameters.attributes.unshift({
+          
           type: ns.ATTRIBUTE_STRIKETHROUGH_STYLE,
-          value: ios ? ns.ATTRIBUTE_UNDERLINE_STYLE_SINGLE : undefined,
+          value: OS_IOS ? ns.ATTRIBUTE_UNDERLINE_STYLE_SINGLE : undefined,
           range: [offset, length]
         });
-
-      } else if (ios && node.name === 'effect') {
+      } 
+      else if (OS_IOS && node.name === 'effect') {
+        
         parameters.attributes.unshift({
+          
           type: ns.ATTRIBUTE_TEXT_EFFECT,
           value: ns.ATTRIBUTE_LETTERPRESS_STYLE,
           range: [offset, length]
         });
-
-      } else if (ios && node.name === 'kern' && node.attribs && node.attribs.value) {
+      } 
+      else if (OS_IOS && node.name === 'kern' && node.attribs && node.attribs.value) {
+        
         parameters.attributes.unshift({
+          
           type: ns.ATTRIBUTE_KERN,
           value: node.attribs.value,
           range: [offset, length]
         });
-
-      } else if (ios && node.name === 'expansion' && node.attribs && node.attribs.value) {
+      } 
+      else if (OS_IOS && node.name === 'expansion' && node.attribs && node.attribs.value) {
+        
         parameters.attributes.unshift({
+          
           type: ns.ATTRIBUTE_EXPANSION,
           value: node.attribs.value,
           range: [offset, length]
         });
-
-      } else if (node.name === 'font' && node.attribs && node.attribs.color) {
+      } 
+      else if (node.name === 'font' && node.attribs && node.attribs.color) {
+        
         parameters.attributes.unshift({
+          
           type: ns.ATTRIBUTE_FOREGROUND_COLOR,
           value: node.attribs.color,
           range: [offset, length]
         });
       }
+      else if (node.name === 'p') {
+			
+		parameters.text += '\n\n';
+	  }
     }
 
     // if we have a font to set
-    if (innerFont) {
-      parameters.attributes.unshift({
-        type: ns.ATTRIBUTE_FONT,
-        value: innerFont,
-        range: [offset, length]
-      });
-    }
+  	innerFont && parameters.attributes.unshift({
+    
+    	type: ns.ATTRIBUTE_FONT,
+    	value: innerFont,
+    	range: [offset, length]
+  	});
   }
 
   return parameters;
 }
 
 module.exports = function(html, callback, customMatcher) {
+  
   var parser = new htmlparser.Parser(new htmlparser.DomHandler(function(error, dom) {
 
     if (error) {
+      
       callback(error);
-
-    } else {
+    } 
+    else {
 
       var parameters = walker({
+        
         type: 'tag',
         children: dom
+        
       }, {
+        
         text: '',
         attributes: []
+        
     }, null, customMatcher);
 
       var attr = ns.createAttributedString(parameters);
@@ -203,5 +240,4 @@ module.exports = function(html, callback, customMatcher) {
   html = html.replace(/<br[^>]*>/gm, '\n');
 
   parser.parseComplete(html);
-
 };
